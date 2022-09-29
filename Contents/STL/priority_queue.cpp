c76dd435121ba3602ec2377f55129619df5122e1
dropjrj <94853802+dropjrj@users.noreply.github.com>
2022-09-29 23:26:58 +0800

    STL

--- Contents/STL/priority_queue.cpp
+++ Contents/STL/priority_queue.cpp
@@ -1,9 +1,9 @@
-#include <bits/stdc++.h> //#include <queue>
+#include <bits/stdc++.h> // #include <queue>
 using namespace std;
 
 int main()
 {
-	priority_queue <int, vector<int>> pq; //greater<int>
+	priority_queue <int, vector<int>> pq; // greater<int>
 	pq.push(1);
 	pq.push(2);
 	pq.push(3);
